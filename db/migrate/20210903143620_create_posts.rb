f879f104aa2121db29d5a148f90f87a00810e917
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-03 23:46:27 +0900

    ファイルを修正して、マイグレーションを実行

--- db/migrate/20210903143620_create_posts.rb
+++ db/migrate/20210903143620_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :content
+      t.string :content, null: false
       t.references :user, null: false, foreign_key: true
-      t.integer :status
-
+      t.integer :status, default: 0, null: false
       t.timestamps
     end
   end

bc12fadc7ffb550ab762ae296859abfd079a4a55
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-03 23:18:46 +0900

    ログイン関連のページを日本語化し，タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module PublicPrivateSample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.default_locale = :ja
+    config.time_zone = "Asis/Tokyo"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

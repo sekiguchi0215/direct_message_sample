63b252a3da04f9b453843a1e4e12984e65036648
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-04 00:26:47 +0900

    enum の記述を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  enum status: { public: 0, private: 1 }
-  # enum status: [:public, :private], _prefix: true
+
+  enum status: [:public, :private], _prefix: true
 end

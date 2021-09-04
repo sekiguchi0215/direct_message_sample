dfaeba508fe59cf7023a81f70f3019ecedbd6343
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-04 23:54:04 +0900

    users コントローラにチャットルームに関連する処理を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,22 @@ class UsersController < ApplicationController
     else
       @posts = @user.posts.where(status: "public")
     end
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+
+    unless @user.id == current_user.index
+      @currentUserEntry.each do |current_user|
+        @userEntry.each do |user|
+          if current_user.room_id == user.room_id
+            @haveRoom = true
+            @roomId = current_user.room_id
+          end
+        end
+      end
+      unless @haveRoom
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 end

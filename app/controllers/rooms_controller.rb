405f0d8081dba7555cf690e28738e81264acdc0c
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-05 18:08:05 +0900

    rooms コントローラに処理を記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,25 @@
 class RoomsController < ApplicationController
   def create
+    @room = Room.create
+    @currentUserEntry = Entry.create(user_id: current_user.id, room_id: @room.id)
+    @userEntry = Entry.create(entry_params)
+    redirect_to room_path(@room.id)
   end
 
   def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redierct_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 end

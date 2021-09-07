2f63845177ab897f927e6c9d12080256e41df66d
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-08 00:00:46 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :messages, only: [:create]
   resources :rooms, only: [:index, :create, :show]
+  resources :groups, only: [:new, :create, :edit, :update]
 end

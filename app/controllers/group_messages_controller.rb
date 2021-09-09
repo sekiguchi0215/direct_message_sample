56d1ab50229eda8034d470fce59472dbfae29515
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-09 18:45:05 +0900

    【途中】グループチャット機能実装

--- app/controllers/group_messages_controller.rb
+++ app/controllers/group_messages_controller.rb
@@ -1,4 +1,16 @@
 class GroupMessagesController < ApplicationController
   def create
+    if GroupUser.where(user_id: current_user.id, group_id: params[:group_message][:group_id]).present?
+      @message = GroupMessage.create(message_params)
+      redirect_to group_path(@message.group_id)
+    else
+      flash[:alert] = "メッセージの送信に失敗しました。"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:group_message).permit(:user_id, :group_id, :content).merge(user_id: current_user.id)
   end
 end

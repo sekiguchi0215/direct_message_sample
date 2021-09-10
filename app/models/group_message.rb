3753c6a747a6adbdc5d7366fbd395016f11b7178
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-10 23:48:41 +0900

    gem を利用して、論理削除を実装

--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -1,4 +1,6 @@
 class GroupMessage < ApplicationRecord
+  acts_as_paranoid
+
   belongs_to :user
   belongs_to :group
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  acts_as_paranoid
+
   belongs_to :user
   belongs_to :room
 

3cd2339af218ca62b6e3b4864985caa7408ef6d7
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-10 23:20:35 +0900

    binding.pry を解除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   def create
-    binding.pry
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
       redirect_to room_path(@message.room_id)

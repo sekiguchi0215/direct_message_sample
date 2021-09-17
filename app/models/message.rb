710d881129e708955ea0ee3a3a1402690685563b
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-18 00:46:09 +0900

    ダイレクトメッセージの通知メソッドを作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,13 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :content, presence: true
+
+  def create_notification_direct_message!(current_user)
+    notification = current_user.active_notifications.new(
+      message_id: id,
+      visited_id: user_id,
+      action: "message",
+    )
+    notification.save if notification.valid?
+  end
 end

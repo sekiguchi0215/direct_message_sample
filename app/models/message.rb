5696aaaf364bad8d975123bb71ba7c6b384e3ee9
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-18 22:23:34 +0900

    ダイレクトメッセージの通知メソッドを修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,11 +5,19 @@ class Message < ApplicationRecord
   validates :content, presence: true
 
   def create_notification_direct_message!(current_user)
-    notification = current_user.active_notifications.new(
-      message_id: id,
-      visited_id: user_id,
-      action: "message",
-    )
-    notification.save if notification.valid?
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and message_id = ? and action = ?", current_user.id, user_id, id, "direct_message"])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        message_id: id,
+        visited_id: user_id,
+        action: "direct_message",
+      )
+
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+
+      notification.save if notification.valid?
+    end
   end
 end

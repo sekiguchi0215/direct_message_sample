c7f44d980211cdcafafa9270cb767c9c51e7317e
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-19 23:55:54 +0900

    通知機能の一覧表示を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
+      @message.create_notification_direct_message!(current_user)
       redirect_to room_path(@message.room_id)
     else
       flash[:alert] = "メッセージの送信に失敗しました。"

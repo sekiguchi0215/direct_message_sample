18483e68e6b4b0c398b35c12d5e1cc8f02e5e307
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-19 21:31:55 +0900

    不備修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :item, optional: true
   belongs_to :message, optional: true
   belongs_to :group_message, optional: true
-  belongs_to :visiter, class_name: "User", foreign_key: "visiter_id", optional: true
+  belongs_to :visitor, class_name: "User", foreign_key: "visiter_id", optional: true
   belongs_to :visited, class_name: "User", foreign_key: "visited_id", optional: true
 end

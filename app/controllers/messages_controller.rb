71a19b4541a4eac51ad2bd163b949517851c070e
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-23 00:20:24 +0900

    ダイレクトメッセージの通知発信機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,10 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      @message.create_notification_direct_message!(current_user)
+      entries = @message.room.entries
+      entry = entries.select(:user_id).where.not(user_id: current_user.id)
+      @visited_id = entry[0][:user_id]
+      @message.create_notification_direct_message!(current_user, @visited_id)
       redirect_to room_path(@message.room_id)
     else
       flash[:alert] = "メッセージの送信に失敗しました。"
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,18 +4,13 @@ class Message < ApplicationRecord
 
   validates :content, presence: true
 
-  def create_notification_direct_message!(current_user)
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and message_id = ? and action = ?", current_user.id, user_id, id, "direct_message"])
+  def create_notification_direct_message!(current_user, visited_id)
+    temp = Notification.where(visitor_id: current_user.id, visited_id: visited_id, action: "direct_message")
     if temp.blank?
       notification = current_user.active_notifications.new(
-        message_id: id,
-        visited_id: user_id,
+        visited_id: visited_id,
         action: "direct_message",
       )
-
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
 
       notification.save if notification.valid?
     end

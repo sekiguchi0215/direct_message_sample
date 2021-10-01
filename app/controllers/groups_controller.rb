cd6e8ff9707d6ddc2063256e981c897d7e8ae5d7
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-01 17:31:01 +0900

    グループへの招待機能・参加機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,8 @@ class GroupsController < ApplicationController
   def show
     @users = @group.users.order(:created_at)
     @group.check_group_message_notification(current_user)
+    group_user_ids = GroupUser.where(group_id: @group.id).pluck(:user_id)
+    @other_users = User.where.not(user_id: group_user_ids)
     unless @users.include?(current_user)
       redirect_to root_path
     end
@@ -41,10 +43,27 @@ class GroupsController < ApplicationController
     end
   end
 
+  def invitation
+    @group = Group.find(params[:id])
+    @user = User.find_by(id: params[:user_id])
+    if @group.group_invitation_notification(current_user, @user.id, @group.id)
+      binding.pry
+      redirect_to request.referer
+    end
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    unless @group.users.include?(current_user)
+      @group.users << current_user
+    end
+    redirect_to group_path(@group)
+  end
+
   private
 
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, :user_id, user_ids: [])
   end
 
   def set_group
--- app/models/group.rb
+++ app/models/group.rb
@@ -8,4 +8,13 @@ class Group < ApplicationRecord
     unchecked_messges = group_messages.where(checked: false).where.not(user_id: current_user)
     unchecked_messges.map { |message| message.update(checked: true) }
   end
+
+  def group_invitation_notification(current_user, visited_id, group_id)
+    notification = current_user.active_notifications.new(
+      visited_id: visited_id,
+      group_id: group_id,
+      action: "invitation",
+    )
+    notification.save if notification.valid?
+  end
 end

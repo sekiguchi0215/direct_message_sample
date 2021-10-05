1ae8ab31f3fe2b47af0e89fdc4465f6fb1ce3447
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-06 00:50:19 +0900

    グループメッセージの通知機能を実装

--- app/controllers/group_messages_controller.rb
+++ app/controllers/group_messages_controller.rb
@@ -1,8 +1,11 @@
 class GroupMessagesController < ApplicationController
   def create
     if GroupUser.where(user_id: current_user.id, group_id: params[:group_message][:group_id]).present?
-      @message = GroupMessage.create(message_params)
-      redirect_to group_path(@message.group_id)
+      message = GroupMessage.create(message_params)
+      visited_ids = message.group.group_users.select(:user_id).where.not(user_id: current_user.id)
+      group_id = message.group_id
+      message.group_message_notification(current_user, visited_ids, group_id)
+      redirect_to group_path(message.group_id)
     else
       flash[:alert] = "メッセージの送信に失敗しました。"
     end
--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -3,4 +3,25 @@ class GroupMessage < ApplicationRecord
   belongs_to :group
 
   validates :content, presence: true
+
+  def group_message_notification(current_user, visited_ids, group_id)
+    visited_ids.each do |visited_id|
+      save_group_message_notification(current_user, visited_id[:user_id], group_id)
+    end
+  end
+
+  def save_group_message_notification(current_user, visited_id, group_id)
+    # group_id = group.id
+    temp = Notification.where(visitor_id: current_user.id, visited_id: visited_id, group_id: group_id)
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: visited_id,
+        group_id: group_id,
+        action: "group_message",
+      )
+      notification.save if notification.valid?
+    else
+      temp.update(checked: false)
+    end
+  end
 end
